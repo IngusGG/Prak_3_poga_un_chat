8fb189af3c57131a193aa472e4d4d64f7647f61d
IngusGG <69913086+IngusGG@users.noreply.github.com>
2020-09-07 17:15:28 +0300

    Pabeidzu... itkā!?

--- app/src/main/java/com/example/prak_3_poga_un_chat/ChatActivity.kt
+++ app/src/main/java/com/example/prak_3_poga_un_chat/ChatActivity.kt
@@ -4,26 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.activity_chat_second_activty.*
 
 class ChatActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_MESSAGE = "lv.romstr.mobile.practice.MESSAGE"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val visible = 0
-        if (visible == 0) {
-            textReceived.visibility = View.INVISIBLE
-            textMessage.visibility = View.INVISIBLE
-        }else{
-            textReceived.visibility = View.VISIBLE
-            textMessage.visibility = View.VISIBLE
-        }
+       // val visible = 0
+       // if (visible == 0) {
+      //      textReceived.visibility = View.INVISIBLE
+       //     textMessage.visibility = View.INVISIBLE
+       // }else{
+       //     textReceived.visibility = View.VISIBLE
+       //     textMessage.visibility = View.VISIBLE
+       // }
 
         ButtonSend.setOnClickListener{
-            val intent = Intent(this, ChatSecondActivty::class.java)
+            val message = editTextTextPersonName2.text.toString()
+            val intent = Intent(this, ChatSecondActivty::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
             startActivity(intent)
+
         }
+
+
+        val message = intent.getStringExtra(ChatSecondActivty.EXTRA_MESSAGE)
+            textMessage.text = message
+
+
+
+
+
 
     }
 }
--- app/src/main/java/com/example/prak_3_poga_un_chat/ChatSecondActivty.kt
+++ app/src/main/java/com/example/prak_3_poga_un_chat/ChatSecondActivty.kt
@@ -8,6 +8,11 @@ import kotlinx.android.synthetic.main.activity_chat.ButtonSend
 import kotlinx.android.synthetic.main.activity_chat_second_activty.*
 
 class ChatSecondActivty : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_MESSAGE = "lv.romstr.mobile.practice.MESSAGE"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_second_activty)
@@ -16,9 +21,16 @@ class ChatSecondActivty : AppCompatActivity() {
         //textMessage2.isClickable = false
 
         ButtonReply.setOnClickListener{
-            val intent = Intent(this, ChatActivity::class.java)
+            val message = editTextTextPersonName.text.toString()
+            val intent = Intent(this, ChatActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
             startActivity(intent)
         }
+
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        textMessage2.text = message
 
     }
 }
